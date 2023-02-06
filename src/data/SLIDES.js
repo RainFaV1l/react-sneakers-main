a1104934c85f7e77ce6cf39ac95494bb301b0d15
Azat <Nazmiev_Flesh_Royal@mail.ru>
2023-02-06 13:39:05 +0300

    Слайдер

---
+++ src/data/SLIDES.js
@@ -0,0 +1,26 @@
+import image from '../assets/banner.png';
+
+const SLIDES = [
+    {
+        id: 1,
+        bacground: "#FFFFFF",
+        url: image,
+        text: "The Guardian",
+        button: {
+            url: '/',
+            text: 'Приобрести'
+        }
+    },
+    {
+        id: 2,
+        bacground: "#FFFFFF",
+        url: "https://media.npr.org/assets/img/2021/02/05/gettyimages-1250276752-832bdef38dd1834d8190b0dafba4049084cd7edf.jpg",
+        text: "The Guardian",
+        button: {
+            url: '/',
+            text: 'Приобрести'
+        }
+    },
+];
+
+export default SLIDES;
